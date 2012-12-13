c70fee5dae514c63c6a82159836f1b5ddd929091
Yasuhisa Yoshida <syou6162@gmail.com>
2012-12-13 20:07:27 +0900

    common lispの基本的な関数をいくつか試した

---
+++ cl/base.lisp
@@ -0,0 +1,36 @@
+nil ; NIL
+(null '()) ; T
+'() ; NIL
+
+;; 基本的な関数を抑える
+(mapcar #'(lambda (x) (+ x 10))
+	'(1 2 3)) ; (11 12 13)
+
+(reduce #'(lambda (result i) (cons i result)) 
+	'(1 2 3) 
+	:initial-value nil) ; (3 2 1)
+
+(defun sum (lst) 
+  (reduce #'(lambda (result x)
+	      (+ result x))
+	  lst
+	  :initial-value 0))
+(sum '(1 2 3)) ; 6
+
+;; consは要素、listの順番
+(cons 0 '(1 2)) ; (0 1 2)
+(length (cons 0 '(1 2))) ; 3
+
+(car '(1 2 3)) ; 1
+(cdr '(1 2 3)) ; (2 3)
+
+(length "日本語") ; 3
+
+;; なんだこの'stringってのは、面倒だな
+(concatenate 'string "abc" "def") ; "abcdef"
+(reverse "日本語") ; "語本日"
+
+(setq japanese "日本語")
+; nが付くと破壊的
+(nreverse japanese) ; "語本日"
+japanese ; "語本日"
