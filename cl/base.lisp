f5276deb68c392ce6012c9cb738021eaa4cd75ab
Yasuhisa Yoshida <syou6162@gmail.com>
2012-12-15 19:33:41 +0900

    map系の関数に無名関数を食わせたりする例を追加

--- cl/base.lisp
+++ cl/base.lisp
@@ -48,3 +48,18 @@ y ; (3 4)
 
 (append '(1 2) '(3 4)) ; (1 2 3 4)
 
+(mapcan (lambda (x) (list (+ x 10) 'x)) '(1 2 3 4))
+; (11 X 12 X 13 X 14 X)
+(mapcan #'list '(a b c d))
+; (A B C D)
+(mapcan (lambda (x) (if (> x 0) (list x) nil)) '(-4 6 -23 1 0 12 ))
+; (6 1 12)
+
+(mapcar (lambda (x) (list (+ x 10) 'x)) '(1 2 3 4))
+; ((11 X) (12 X) (13 X) (14 X))
+
+(mapcar #'list '(a b c d))
+; ((A) (B) (C) (D))
+(mapcar (lambda (x) (if (> x 0) (list x) nil)) '(-4 6 -23 1 0 12 ))
+; (NIL (6) NIL (1) NIL (12))
+
